73b5cd95963970be5de860930788fcb43fa09b24
llius123 <llius619@gmail.com>
2018-10-22 13:30:49 +0200

    Loggin sync 'Creo'
    Creo que el apartado loggin esta hecho de forma syncrona.
    -->NO SE SI ESTA BIEN<---

--- src/server/Loggin/loggin.firebase.js
+++ src/server/Loggin/loggin.firebase.js
@@ -15,36 +15,39 @@ exports.createUser = function (app, firebase) {
 }
 
 exports.loggin = function (app, firebase) {
-    app.get('/loggin/:user/:pass', (req, res) => {
-        firebase.auth().signInWithEmailAndPassword(req.params.user, req.params.pass)
+    app.get('/loggin/:user/:pass', async function (req, res) {
+
+        const auth = await firebase.auth().signInWithEmailAndPassword(req.params.user, req.params.pass)
             .catch(function (err) {
                 var errorCode = err.code;
                 var errorMessage = err.message;
             });
-        // firebase.auth().onAuthStateChanged(function (user) {
-        //     if (user) {
-        //         // User is signed in.
-        //         var displayName = user.displayName;
-        //         var email = user.email;
-        //         var emailVerified = user.emailVerified;
-        //         var photoURL = user.photoURL;
-        //         var isAnonymous = user.isAnonymous;
-        //         var uid = user.uid;
-        //         var providerData = user.providerData;
-        //         const userLogged = {
-        //             nombre : displayName,
-        //             email : email 
-        //         }
-        //         res.send(userLogged);
-        //         res.end();
-        //     } else {
-        //         mensaje = { 'msg': 'No existe el usuario'};
-        //         res.send(mensaje);
-        //         res.end();
-        //     }
-        // });
-        res.end();
-    })
+        const check = await firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                // User is signed in.
+                var displayName = user.displayName;
+                var email = user.email;
+                var emailVerified = user.emailVerified;
+                var photoURL = user.photoURL;
+                var isAnonymous = user.isAnonymous;
+                var uid = user.uid;
+                var providerData = user.providerData;
+                const userLogged = {
+                    nombre: displayName,
+                    email: email
+                }
+                res.send(userLogged);
+                res.end();
+            } else {
+                mensaje = { 'msg': 'No existe el usuario' };
+                res.send(mensaje);
+                res.end();
+            }
+        });
+        auth;
+        check;
+    }
+    )
 }
 
 exports.loggout = function (app, firebase) {
