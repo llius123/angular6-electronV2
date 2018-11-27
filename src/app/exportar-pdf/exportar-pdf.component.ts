ee840e8e96fb8cc5fd833b0a54218822c420faa5
llius123 <llius619@gmail.com>
2018-11-27 09:17:52 +0100

    Fallo seguridad arreglado

--- src/app/exportar-pdf/exportar-pdf.component.ts
+++ src/app/exportar-pdf/exportar-pdf.component.ts
@@ -6,29 +6,38 @@ import { Factura } from '../extras/factura.service';
 
 import $ from "jquery";
 import * as jsPDF from 'jspdf'
+import { AngularFireDatabase } from '../../../node_modules/@angular/fire/database';
 @Component({
   selector: 'app-exportar-pdf',
   templateUrl: './exportar-pdf.component.html',
-  styleUrls: ['./exportar-pdf.component.css']
+  styleUrls: ['./exportar-pdf.component.css'],
+  providers: [AngularFireDatabase]
 })
 export class ExportarPdfComponent implements OnInit {
 
-  constructor(private router: Router, private cFactura: Factura) { }
+  constructor(private router: Router, private cFactura: Factura,private db: AngularFireDatabase) { }
 
   ngOnInit() {
     this.factura = this.cFactura.getFactura();
-    this.validacion();
+    //this.validacion();
    }
 
   factura: iFactura;
 
   crear_factura() {
-    let doc = new jsPDF('p', 'pt', 'a4');
-    doc.addHTML(document.getElementById("pdf")).then(canvas => {
-      var img = canvas.toDataURL("image/jpeg", 1.0);
-      doc.addImage(img, 'JPEG', 0, 0);
-      doc.save('html.pdf');
+    this.db.database.goOnline();
+    const test = this.db.database.app.database().ref("usuarios")
+    test.on("value", (snapshot) => {
+      console.log(snapshot.val);
+    }, (errorObject) => {
+      console.log(errorObject)
     })
+    // let doc = new jsPDF('p', 'pt', 'a4');
+    // doc.addHTML(document.getElementById("pdf")).then(canvas => {
+    //   var img = canvas.toDataURL("image/jpeg", 1.0);
+    //   doc.addImage(img, 'JPEG', 0, 0);
+    //   doc.save('html.pdf');
+    // })
   }
 
   validacion(){
